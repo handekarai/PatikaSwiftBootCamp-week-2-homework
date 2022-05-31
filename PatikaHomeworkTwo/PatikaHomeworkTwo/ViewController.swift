a6cf1847900f517e3dedae8cf3ddbcbc6397e6c7
handekarai <handekarai.hk@gmail.com>
2022-05-31 17:47:41 +0300

    first commit

---
+++ PatikaHomeworkTwo/PatikaHomeworkTwo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PatikaHomeworkTwo
+//
+//  Created by Hande Kara on 5/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
