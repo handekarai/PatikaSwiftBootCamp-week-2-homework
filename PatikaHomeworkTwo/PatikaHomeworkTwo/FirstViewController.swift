113b7d1e711f5cb06687aac968feb8d671333326
handekarai <handekarai.hk@gmail.com>
2022-06-01 16:52:40 +0300

    commits added

--- PatikaHomeworkTwo/PatikaHomeworkTwo/FirstViewController.swift
+++ PatikaHomeworkTwo/PatikaHomeworkTwo/FirstViewController.swift
@@ -7,41 +7,39 @@
 
 import UIKit
 
+///This class is about first page when app starts
 class FirstViewController: UIViewController {
-   
-    
-    var name: String? = nil
-    @IBOutlet weak var nameTextLabel: UILabel!
-    
-
-    @IBOutlet weak var formButton: UIButton!
+       
+    @IBOutlet weak var nameTextLabel: UILabel!                                                      //Hello Label
+    @IBOutlet weak var formButton: UIButton!                                                        //Go to Form Button
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameTextLabel.text = name ?? "Hello Nobody"
-        formButton.layer.borderWidth = 3
-        formButton.layer.borderColor = UIColor.systemIndigo.cgColor
+        nameTextLabel.text = "Hello Nobody"                                                         //User sees at beginning
+        formButton.layer.borderWidth = 3                                                            //Defines button's border thickness
+        formButton.layer.borderColor = UIColor.systemIndigo.cgColor                                 //Defines button's border color
         
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(submitSucces),
-                                               name: .nameNotification,
+                                               selector: #selector(submitSucces),                   //Observes the notification
+                                               name: .nameNotification,                             //from .nameNotification center
                                                object: nil)
     }
     
+    ///When go to form is pressed, then the form page opens from bottom of app to up
     @IBAction func formButtonPressed(_ sender: Any) {
         
-        if let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController{
+        if let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(
+            withIdentifier: "SecondViewController") as? SecondViewController{
             self.present(vc, animated: true) {
             }
         }
-        
     }
     
+    ///Submit button triggers this function, hello label is uptated according to name
     @objc func submitSucces(_ notification:Notification){
-        let nameFromForm = notification.userInfo!["name"] as! String
+        guard let nameFromForm = notification.userInfo?["name"] as? String else { return }
         self.nameTextLabel.text = "Hello \(nameFromForm)"
-        
     }
 
 }
--- PatikaHomeworkTwo/PatikaHomeworkTwo/SecondViewController.swift
+++ PatikaHomeworkTwo/PatikaHomeworkTwo/SecondViewController.swift
@@ -7,35 +7,39 @@
 
 import UIKit
 
+
+///This class is about form page that is second page of app
 class SecondViewController: UIViewController {
 
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var surnameTextField: UITextField!
-    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!                                                                  // first text field that is name in page
+    @IBOutlet weak var surnameTextField: UITextField!                                                               //second text field that is surname in page
+    @IBOutlet weak var submitButton: UIButton!                                                                      //the submit button for form
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameTextField.layer.borderColor = UIColor.orange.cgColor
-        nameTextField.layer.borderWidth = 2
+        nameTextField.layer.borderColor = UIColor.orange.cgColor                                                    //color of name text field border
+        nameTextField.layer.borderWidth = 2                                                                         //border thickness
         surnameTextField.layer.borderColor = UIColor.orange.cgColor
         surnameTextField.layer.borderWidth = 2
         submitButton.layer.borderWidth = 3
         submitButton.layer.borderColor = UIColor.systemIndigo.cgColor
     }
 
+    ///submit button triggers it , get the name from text fields and post it notification center, end page is closed
     @IBAction func submitButtonPressed(_ sender: Any) {
         
-        let fullName = "\(self.nameTextField.text ?? "Nobody") \(self.surnameTextField.text ?? "Nobody")"
+        let fullName = "\(self.nameTextField.text ?? "Nobody") \(self.surnameTextField.text ?? "Nobody")"           // get full name from text fields
         
         NotificationCenter.default.post(name: .nameNotification,
-                                        object: nil, userInfo: ["name":fullName])
+                                        object: nil, userInfo: ["name":fullName])                                   //post notification for full name of user
         
-        self.dismiss(animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)                                                               //for closing second page and go back first
 
     }
     
 }
 
+///extension for identifiying the notification center
 extension NSNotification.Name{
     static let nameNotification = Notification.Name.init(rawValue: "nameNotification")
 }
